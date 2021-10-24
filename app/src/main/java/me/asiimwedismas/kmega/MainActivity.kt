c54379ac10052febbce1fe9f99192b41635d37b4
asiimwedismas <asiimwedismas@gmail.com>
2021-10-24 16:41:49 +0300

    Initial commit

---
+++ app/src/main/java/me/asiimwedismas/kmega/MainActivity.kt
@@ -0,0 +1,38 @@
+package me.asiimwedismas.kmega
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import me.asiimwedismas.kmega.ui.theme.KmegaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KmegaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KmegaTheme {
+        Greeting("Android")
+    }
+}
